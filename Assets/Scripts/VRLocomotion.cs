d44b0a857f8c38385c1eacbbffd559a64ffe4aa3
Tyrell-CircuitStream <tyrell.lewin@gmail.com>
2021-02-03 11:24:19 -0800

    Initial project

---
+++ Assets/Scripts/VRLocomotion.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRLocomotion : MonoBehaviour
+{
+    //[Header("Teleport")]
+
+
+    //[Header("Curved Line")]
+  
+
+    //[Header("Smooth Reticle")]
+ 
+
+    //[Header("Fade Screen")]
+
+
+    // Internal Vars
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void InitializeLineRenderer()
+    {
+
+    }
+
+    private void HandleRaycast()
+    {
+        
+    }
+
+    private void Rotate()
+    {
+        
+    }
+
+    private IEnumerator FadeTeleport(Vector3 pos)
+    {
+        // Make sure teleport can't be called again
+
+        // Reset Time counter variable
+
+        // Loop until counter is done
+        // Fade out screen
+
+        // Wait one frame
+
+        // Increment Timer
+
+        // Set full black screen
+
+        // Move user
+
+        // Wait one second
+
+        // Reset timer again
+
+        // Loop until timer is done again
+        // Fade in screen
+
+        // Wait one frame
+
+        // Increment Timer
+
+        // Allow teleporting again
+
+        yield return null;
+    }
+}
